3b516b2ae19439cf693ba0ea2930e1b4c091106b
daisuke sasaki <gerund25495641@gmail.com>
2020-12-25 23:27:45 +0900

    add jQuery animation.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+$(".my-job").css({
+  left:"2000px",
+  opacity:"0.0"
+}).animate({
+  left: "1128px",
+  opacity:"1.0"
+},1000);
 
+$(".my-name").css({
+  left:"2000px",
+  opacity:"0.0"
+}).animate({
+  left: "680px",
+  opacity:"1.0"
+},1000);
+
+
+$(".header-top button").css({
+  left:"2000px",
+  opacity:"0.0"
+}).animate({
+  left: "998px",
+  opacity:"1.0"
+},1000);
